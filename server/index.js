bd07913f67db4085781297cc07629d5f3bc6dc5e
José Oscátegui <jose.osk01@gmail.com>
2018-08-14 11:57:42 -0500

    remembering

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import yields from 'express-yields';
+import fs from 'fs-extra';
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.get(['/'], function* (req, res) {
+  const index = yield fs.readFile('./public/index.html', 'utf-8');
+  res.send(index);
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.info(`App listening on ${PORT}`);
+});
