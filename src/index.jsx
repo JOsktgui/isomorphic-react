b8657811b83c25f738f49e1146e092caabc5aa43
José Oscátegui <jose.osk01@gmail.com>
2018-08-15 11:53:44 -0500

    implementing react and react-redux

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import getStore from './getStore';
+import { Provider } from 'react-redux';
+
+const store = getStore();
+
+const render = _App => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <_App />
+    </Provider>,
+    document.getElementById('app-container')
+  );
+};
+
+render(App);
